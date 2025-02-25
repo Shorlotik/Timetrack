b5d877bea9b3f497378205eb58031113a9113f90
maksimprokopchik <maksimprokopchik03@gmail.com>
2025-02-25 11:33:36 +0300

    Implement dotenv loading in TimeTrackApplication

--- src/main/java/org/example/timetrack/TimeTrackApplication.java
+++ src/main/java/org/example/timetrack/TimeTrackApplication.java
@@ -2,11 +2,19 @@ package org.example.timetrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TimeTrackApplication {
 
     public static void main(String[] args) {
+        // Загружаем переменные из .env файла
+        Dotenv dotenv = Dotenv.load();
+
+        // Устанавливаем переменные окружения
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
         SpringApplication.run(TimeTrackApplication.class, args);
     }
 }
