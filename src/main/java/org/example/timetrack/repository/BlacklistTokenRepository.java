bdbe438158580855993fcda631d6fd9bdf069cd3
maksimprokopchik <maksimprokopchik03@gmail.com>
2025-02-28 16:14:35 +0300

    deleted

--- src/main/java/org/example/timetrack/repository/BlacklistTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.timetrack.repository;
-
-import org.example.timetrack.entity.BlacklistedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlacklistTokenRepository extends JpaRepository<BlacklistedToken, Long> {
-    boolean existsByToken(String token);
-}
