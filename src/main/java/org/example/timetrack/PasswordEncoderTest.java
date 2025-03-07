1b49922d63b5f6a121cbb38f428ffa1028de54c0
maksimprokopchik <maksimprokopchik03@gmail.com>
2025-03-07 11:35:23 +0300

    Fix bugs

--- src/main/java/org/example/timetrack/PasswordEncoderTest.java
+++ src/main/java/org/example/timetrack/PasswordEncoderTest.java
@@ -1,12 +1,24 @@
 package org.example.timetrack;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.Key;
+import java.util.Base64;
 
 public class PasswordEncoderTest {
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+        // Генерация JWT_SECRET
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String jwtSecret = Base64.getEncoder().encodeToString(key.getEncoded());
+
+        System.out.println("Сгенерированный JWT_SECRET: " + jwtSecret);
+
+        // Хеширование пароля
         String rawPassword = "password456";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
