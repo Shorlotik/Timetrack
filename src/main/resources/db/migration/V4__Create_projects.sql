5041149bf1b20fb8ed9f5ac268b32f1f27154443
Prokopchik Maksim <145142168+Shorlotik@users.noreply.github.com>
2025-03-17 14:02:43 +0300

    Fix bugs

--- src/main/resources/db/migration/V4__Create_projects.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE projects (
-                          id BIGSERIAL PRIMARY KEY,
-                          name VARCHAR(255) NOT NULL UNIQUE,
-                          description TEXT,
-                          created_by BIGINT NOT NULL,
-                          CONSTRAINT fk_projects_user FOREIGN KEY (created_by) REFERENCES users (id) ON DELETE CASCADE
-);
